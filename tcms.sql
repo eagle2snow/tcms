a002e7f0a928fe68d9d275677f7259d83de96bda
Administrator <Administrator@AUTOBVT-DHFBU4C>
2017-12-14 15:23:15 +0800

    导入正确的 tcms.sql 数据库执行文件

---
+++ tcms.sql
@@ -0,0 +1,57 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.1.62-community : Database - tcms
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tcms` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `tcms`;
+
+/*Table structure for table `course` */
+
+DROP TABLE IF EXISTS `course`;
+
+CREATE TABLE `course` (
+  `cno` varchar(20) NOT NULL,
+  `cname` varchar(20) NOT NULL,
+  `tno` varchar(20) NOT NULL,
+  PRIMARY KEY (`cno`),
+  KEY `tno` (`tno`),
+  CONSTRAINT `course_ibfk_1` FOREIGN KEY (`tno`) REFERENCES `teacher` (`tno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `course` */
+
+insert  into `course`(`cno`,`cname`,`tno`) values ('3-105','计算机导论','825'),('3-245','操作系统','804'),('6-166','数字电路','856'),('9-123','python','804'),('9-234','Java','804'),('9-888','高等数学','831');
+
+/*Table structure for table `teacher` */
+
+DROP TABLE IF EXISTS `teacher`;
+
+CREATE TABLE `teacher` (
+  `tno` varchar(20) NOT NULL,
+  `tname` varchar(20) NOT NULL,
+  `tsex` varchar(20) NOT NULL,
+  `tbirthday` datetime DEFAULT NULL,
+  `prof` varchar(20) DEFAULT NULL,
+  `depart` varchar(20) NOT NULL,
+  PRIMARY KEY (`tno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `teacher` */
+
+insert  into `teacher`(`tno`,`tname`,`tsex`,`tbirthday`,`prof`,`depart`) values ('804','李诚','男','1958-12-02 00:00:00','副教授','计算机系'),('825','王萍','女','1972-05-05 00:00:00','助教','计算机系'),('831','刘冰','女','1977-08-14 00:00:00','助教','电子工程系'),('856','张旭','男','1969-03-12 00:00:00','讲师','电子工程系');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
